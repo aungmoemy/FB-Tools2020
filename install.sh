6f8f4f1a0622dcf3c83c260a58f0b619e83bedad
aungmoemy <72740033+aungmoemy@users.noreply.github.com>
2020-10-15 02:40:31 +0630

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+apt install ruby -y
+gem install lolcat -y
+apt install python -y
+apt install python2 -y
+apt install git -y
+apt install openssh -y
+apt install wget -y
+apt install php -y
+pip2 install requests
+pip2 install mechanize
+clear
+echo '            Seccssful Install ' | lolcat -a -d 30
+sleep 2
+clear
+echo '         TOOL USARNAME =fatabotol ' | lolcat -a -d 10
+echo '         TOOL PASSWORD =fatabotol ' | lolcat -a -d 10
+echo '         ---------------------------------------------'
+echo -e '\e[31;m                        ! Copy Tool !\e[0m '
+sleep 2
+echo '         Org = https://github.com/BotolMehedi/bint.git ' | lolcat -a -d 30  
+
